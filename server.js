2dcf54d81d2d710e97422dd6a356933ba9f29c82
Daniel Swann <swann.dp@gmail.com>
2022-03-08 19:20:43 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// Perform imports
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+// Set constants
+const PORT = process.env.PORT || 5000;
+
+// Create express app
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => 
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) =>
+    {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () =>
+{
+    console.log('Server listening on port ' + PORT);
+});
+
