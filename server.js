97cac3af48e342d5b7a09cea2577a0c87fb78ba8
Daniel Swann <swann.dp@gmail.com>
2022-03-09 15:37:42 -0500

    Move constants to their own file for easy import

--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const path = require('path');
 
 
 // Set constants
-const PORT = process.env.PORT || 5000;
+const C = require('./constants.js');
 
 
 // Create and configure MongoDB connection
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-const url = process.env.MONGODB_URI;
+const url = C.MONGODB_URI;
 const client = new MongoClient(url);
 client.connect();
 
 
 // Create and configure express app
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', C.PORT);
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -52,7 +51,7 @@ apiMain.setApp(app, client);
 
 
 // Begin listening on relevant port
-app.listen(PORT, () =>
+app.listen(C.PORT, () =>
 {
-    console.log('Server listening on port ' + PORT);
+    console.log('Server listening on port ' + C.PORT);
 });
