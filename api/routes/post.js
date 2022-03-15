d476604830425caa2d3f494d3b403ff88a75cea3
Jinan Patel <pateljinan@gmail.com>
2022-03-14 23:56:17 -0400

    Comment on post endpoint

--- api/routes/post.js
+++ api/routes/post.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postController = require('../controllers/post');
 router.post('/newPost', postController.newPost);
 router.post('/likePost', postController.likePost);
+router.post('/commentOnPost', postController.commentOnPost);
 
 module.exports = router;
