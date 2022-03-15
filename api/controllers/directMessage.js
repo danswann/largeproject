e8555620ba7ae5b7a8b1e441c9b859d356687190
Sebastian Scharager <sscharager@gmail.com>
2022-03-14 23:57:03 -0400

    Added new dm group api

--- api/controllers/directMessage.js
+++ api/controllers/directMessage.js
@@ -1,5 +1,21 @@
 const DirectMessage = require('../models/directMessage');
 
+function checkObjectId (id) {
+    const ObjectId = require('mongoose').Types.ObjectId;
+
+    if(ObjectId.isValid(id)) {
+        if((String)(new ObjectId(id)) === id)
+        {
+            return true;
+        }
+        return false;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 exports.newChat = async function(req, res, next) {
     // Default response object
     var response = {ok:true};
@@ -7,10 +23,23 @@ exports.newChat = async function(req, res, next) {
     // Incoming values
     const {users} = req.body;
 
+    // Check if userID is a valid object id
+    for (let i = 0; i < users.length; i++)
+    {
+        if(!checkObjectId(users[i])) {
+            response.ok = false;
+            response.error = 'Invalid user id';
+            res.status(200).json(response);
+            return;
+        }
+    }
+
+    // Create new direct message group
     var newDM = new DirectMessage({
         users: users
     });
 
+    // Save to database
     newDM.save(function (err) {
         if(err)
         {
