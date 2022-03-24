f9545f84845c75f2f4502b138ce8cb4a6afed5fc
Douglas <douglasglover3@knights.ucf.edu>
2022-03-23 21:55:48 -0400

    Register screen now requires valid inputs

--- mobileApp/screens/UnauthenticatedScreen.js
+++ mobileApp/screens/UnauthenticatedScreen.js
@@ -18,6 +18,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContext } from "../Context";
 
+
+
 // Login screen
 function LoginScreen({ navigation }) {
   // Keeps track of what the user has inputted
@@ -123,157 +125,134 @@ function RegisterScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhone] = useState("");
   const [dob, setDob] = useState("");
-
+  const [registerState, setRegisterState] = useState("");
   const { signUp } = React.useContext(AuthContext);
+
+  function verifyRegisterData(username, email, password, confirmPassword)
+  {
+    if (username === "")
+      return "Username field must be filled"
+    else if (username.length < 4)
+      return "Username is too short (At least 3 characters)"
+    else if (email === "")
+      return "Email field must be filled"
+    else if (!verifyEmailFormat(email)) 
+      return "Email format is invalid"
+    else if (password === "")
+      return "Password field must be filled"
+    else if(password !== confirmPassword)
+      return "Passwords do not match"
+    else
+      signUp("", "", email, "", username, password, "")
+      return "Verified"
+  }
+  function verifyEmailFormat(email) //verifies format of string@string.string
+  {
+    var reg = /\S+@\S+\.\S+/;
+    return reg.test(email);
+  }
 
   return (
     // Pushes content up when keyboard is blocking it
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      {/* Makes screen scrollable and closes keyboard when pressing off */}
-      <ScrollView keyboardShouldPersistTaps={"never"} bounces={false}>
-        {/* // Main container */}
-        <View style={styles.container}>
-          {/* Soundlink logo */}
-          <Image
-            style={styles.image}
-            resizeMethod="resize"
-            resizeMode="contain"
-            source={require("../assets/images/soundlinklogo.png")}
+    // Main container
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {/* // Main container */}
+      <View style={styles.container}>
+        {/* Soundlink logo */}
+        <Image
+          style={styles.image}
+          resizeMethod="resize"
+          resizeMode="contain"
+          source={require("../assets/images/soundlinklogo.png")}
+        />
+
+        <StatusBar style="auto" />
+        {/* Username input field */}
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Username"
+            placeholderTextColor="white"
+            clearButtonMode="while-editing"
+            selectionColor={"#573C6B"}
+            onChangeText={(username) => setUsername(username)}
           />
-
-          <StatusBar style="auto" />
-          {/* Username input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Username"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              onChangeText={(username) => setUsername(username)}
-            />
-          </View>
-          {/* Email Address input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Email Address"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              autoCompleteType="email"
-              onChangeText={(email) => setEmail(email)}
-            />
-          </View>
-
-          {/* Password input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Password"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              secureTextEntry={true}
-              onChangeText={(password) => setPassword(password)}
-            />
-          </View>
-
-          {/* Confirm Password input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Confirm Password"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              secureTextEntry={true}
-              onChangeText={(confirmPassword) =>
-                setConfirmPassword(confirmPassword)
-              }
-            />
-          </View>
-
-          {/* Name input fields */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="First Name"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              onChangeText={(firstName) => setFirstName(firstName)}
-            />
-          </View>
-
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Last Name"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              onChangeText={(lastName) => setLastName(lastName)}
-            />
-          </View>
-
-          {/* Phone number input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Phone Number"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              onChangeText={(phoneNumber) => setPhone(phoneNumber)}
-            />
-          </View>
-
-          {/* Date of birth input field */}
-          <View style={styles.inputView}>
-            <TextInput
-              style={styles.TextInput}
-              placeholder="Date of birth"
-              placeholderTextColor="white"
-              clearButtonMode="while-editing"
-              selectionColor={"#573C6B"}
-              onChangeText={(dob) => setDob(dob)}
-            />
-          </View>
-
-          {/* Register button */}
-          <TouchableOpacity
-            style={styles.loginBtn}
-            onPress={() =>
-              signUp(
-                firstName,
-                lastName,
-                email,
-                phoneNumber,
-                username,
-                password,
-                dob
-              )
-            }
-          >
-            <Text style={styles.loginText}>Register</Text>
-          </TouchableOpacity>
-
-          {/* Back to login */}
-          <View style={styles.backToLoginView}>
-            <Text
-              onPress={() => navigation.navigate("Login")}
-              style={styles.signUpText}
-            >
-              Already have an account?{" "}
-              <Text style={styles.clickableText}>Login</Text>
-            </Text>
-          </View>
         </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        {/* Email Address input field */}
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email Address"
+            placeholderTextColor="white"
+            clearButtonMode="while-editing"
+            selectionColor={"#573C6B"}
+            autoCompleteType="email"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+
+        {/* Password input field */}
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            placeholderTextColor="white"
+            clearButtonMode="while-editing"
+            selectionColor={"#573C6B"}
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+
+        {/* Confirm Password input field */}
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Confirm Password"
+            placeholderTextColor="white"
+            clearButtonMode="while-editing"
+            selectionColor={"#573C6B"}
+            secureTextEntry={true}
+            onChangeText={(confirmPassword) =>
+              setConfirmPassword(confirmPassword)
+            }
+          />
+        </View>
+
+
+        {/* Register button */}
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={async () => setRegisterState(await verifyRegisterData(
+              username,
+              email,
+              password,
+              confirmPassword
+            ))
+          }
+        >
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableOpacity>
+
+        {/* Conditionally renders error message if user enteres invalid login credentials */}
+        {registerState === "Verified" ? (
+        <Text></Text>
+        ) : (
+        <Text style={styles.errorText}>{registerState}</Text>
+        )}
+
+        {/* Back to login */}
+        <View style={styles.backToLoginView}>
+          <Text
+            onPress={() => navigation.navigate("Login")}
+            style={styles.signUpText}
+          >
+            Already have an account?{" "}
+            <Text style={styles.clickableText}>Login</Text>
+          </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -354,6 +333,7 @@ const styles = StyleSheet.create({
   },
 
   errorText: {
+    marginTop: 10,
     color: "red",
   },
 
