a224b843219f6e1572496064a21fab0975ce9631
WillOrtiz7 <willortiz407@gmail.com>
2022-03-30 12:37:20 -0400

    Merge

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-
+require("dotenv").config();
 
 exports.PORT = process.env.PORT || 5000;
 exports.MONGODB_URI = process.env.MONGODB_URI;
-exports.SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
-exports.SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+exports.SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+exports.SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
