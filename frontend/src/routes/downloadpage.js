432bb07d8e4d34f690c37042e1af57a580de3b70
jasonmlam <jasonlam@knights.ucf.edu>
2022-04-17 23:59:16 -0400

    website changes

--- frontend/src/routes/downloadpage.js
+++ frontend/src/routes/downloadpage.js
@@ -1,5 +1,5 @@
 import '../App.css'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { useNav } from '../hooks/useNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,21 +7,28 @@ const DownloadPage = () => {
     const aboutRef = useNav('DownloadPage');
 
     return (
-		<main ref={aboutRef} id='downloadContainer'>
+		<main ref={aboutRef} id='downloadContainer' style={{marginBottom: 300}}>
 			<Container style={{justifyContent:'center', display:'flex'}}>
 				<Row className="container mt-5">
-					{/* Image */}
-					<Col>
+					{/* download buttons */}
+					<Col style={{justifyContent:'center', display:'flex', flexDirection:'column', alignItems:'center'}}>
 						<img
-							src='https://source.unsplash.com/random/600x600/?nature,water'							
-							width="75%"
-							alt='unsplash-img'							
+							src='/assets/downloadAppStore.png'					
+							height="250px"
+							width="250px"
+							alt=""
+						/>
+						<img
+							src='/assets/downloadGooglePlay.png'
+							height="250px"
+							width="250px"
+							alt=""
 						/>
 					</Col>
 					{/* body text */}
 					<Col>				
-						<h1 className="description mt-4">download our app</h1>
-						<p style={{color: 'white'}}>click here to download</p>				
+						<h1 className="mt-4" style={{color:"#A57FC1", textAlign: 'right'}}>download our app</h1>
+						<p className="mt-5" style={{textAlign: 'right', color: 'white'}}>click here to download</p>				
 					</Col>
 				</Row>
 			</Container>
--- frontend/src/routes/homepage.js
+++ frontend/src/routes/homepage.js
@@ -4,36 +4,10 @@ import { useNav } from '../hooks/useNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserView, MobileView } from 'react-device-detect';
 
-
 function HomePage() {
     const aboutRef = useNav('HomePage');
     return (
-        <main ref={aboutRef} id='homeContainer'>
-            {/* navbar */}
-            {/* <Navbar class="navbar navbar-expand-lg navbar-dark" variant="dark">
-                <Container>
-                    <NavLink class="navbar-brand" href="#"><img src="/assets/soundlinklogo.png" width="50%" alt=""/></NavLink>
-                    <button 
-                        class="navbar-toggler" 
-                        type="button" 
-                        variant="flat"
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarNavAltMarkup" 
-                        aria-controls="navbarNavAltMarkup" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <Container class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <Container className="navbar-nav justify-content-end">
-                            <NavLink className="nav-link active" aria-current="page" href="#">home</NavLink>
-                            <NavLink className="nav-link" href="#">about us</NavLink>
-                            <NavLink className="nav-link" href="#">contact</NavLink>
-                            <NavLink className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">download</NavLink>
-                        </Container>
-                    </Container>         
-                </Container>
-            </Navbar> */}
+        <main ref={aboutRef} id='homeContainer' style={{marginTop: 100, marginBottom: 300}}>
             {/* description */}
             <BrowserView>
                 <Container style={{justifyContent:'center', display:'flex'}}>
@@ -42,7 +16,7 @@ function HomePage() {
                         <Col>
                             <h1 className="description text-left text-bold">discover new sounds</h1>        
                             <p className="mt-5" style={{color:'white'}}>share your playlists with the world</p>
-                            <p className="mt-5" style={{color:'white'}}>join soundlink today</p>
+                            <p style={{color:'white'}}>join soundlink today</p>
                         </Col>
                         {/* phone screen image */}
                         <Col>
@@ -52,7 +26,7 @@ function HomePage() {
                                 alt=""                            
                             />
                         </Col>
-                    </Row>
+                    </Row>                
                 </Container>
             </BrowserView>
             <MobileView>
@@ -65,3 +39,28 @@ function HomePage() {
 
 export default HomePage;
 
+// {/* navbar */}
+// {/* <Navbar class="navbar navbar-expand-lg navbar-dark" variant="dark">
+//     <Container>
+//         <NavLink class="navbar-brand" href="#"><img src="/assets/soundlinklogo.png" width="50%" alt=""/></NavLink>
+//         <button 
+//             class="navbar-toggler" 
+//             type="button" 
+//             variant="flat"
+//             data-bs-toggle="collapse" 
+//             data-bs-target="#navbarNavAltMarkup" 
+//             aria-controls="navbarNavAltMarkup" 
+//             aria-expanded="false" 
+//             aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <Container class="collapse navbar-collapse" id="navbarNavAltMarkup">
+//             <Container className="navbar-nav justify-content-end">
+//                 <NavLink className="nav-link active" aria-current="page" href="#">home</NavLink>
+//                 <NavLink className="nav-link" href="#">about us</NavLink>
+//                 <NavLink className="nav-link" href="#">contact</NavLink>
+//                 <NavLink className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">download</NavLink>
+//             </Container>
+//         </Container>         
+//     </Container>
+// </Navbar> */}
